ef2dad6a92154ef483efc3237cd03ad161b44110
PRBI10231 <ext.rami.ben-ikhelef@sncf.fr>
2026-03-10 16:19:35 +0100

    correction airflow

--- terraform-airflow/airflow.sh
+++ terraform-airflow/airflow.sh
@@ -51,13 +51,13 @@ source /opt/airflow/venv/bin/activate
 pip install --upgrade pip setuptools wheel
 
 PYTHON_MINOR=$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
-CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_MINOR}.txt"
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-$${PYTHON_MINOR}.txt"
 
-pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "$${CONSTRAINT_URL}"
 
 export AIRFLOW_HOME=/opt/airflow/airflow-home
-mkdir -p "${AIRFLOW_HOME}"
-mkdir -p "${AIRFLOW_HOME}/dags" "${AIRFLOW_HOME}/logs" "${AIRFLOW_HOME}/plugins"
+mkdir -p "$${AIRFLOW_HOME}"
+mkdir -p "$${AIRFLOW_HOME}/dags" "$${AIRFLOW_HOME}/logs" "$${AIRFLOW_HOME}/plugins"
 
 airflow db migrate
 

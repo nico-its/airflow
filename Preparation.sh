c3deb30580d4000af7fe5b58106b7107f0ac1fe1
Ben Ikhelef Ramy <85173815+Ramy-BenIkhelef@users.noreply.github.com>
2026-03-10 11:42:27 +0100

    Update Preparation.sh

--- Preparation.sh
+++ Preparation.sh
@@ -3,6 +3,4 @@
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo 
 sudo yum install terraform -y
-sudo mkdir /home/airflow
-cd /home/airflow
-git clone https://github.com/nico-its/airflow.git
+

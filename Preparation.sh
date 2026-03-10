d562b03feede174b89d3c5c22a4ed955e6b7ce3d
PRBI10231 <ext.rami.ben-ikhelef@sncf.fr>
2026-03-10 17:36:23 +0100

    sudo

--- Preparation.sh
+++ Preparation.sh
@@ -5,5 +5,5 @@ sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinu
 sudo yum install terraform -y
 cd terraform-airflow
 sudo terraform init
-terraform plan -out=plan.tfplan
-terraform apply -auto-approve plan.tfplan
+sudo terraform plan -out=plan.tfplan
+sudo terraform apply -auto-approve plan.tfplan
